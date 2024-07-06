f8bffdde18fcd631c5b6f24f643332b71bea27a3
khaipro09 <nguyenkhai1206200325@gmail.com>
2024-07-06 12:50:18 +0700

    test

--- ShopBanGiay/ShopBanGiay.Module/obj/Debug/net8.0/ShopBanGiay.Module.AssemblyInfo.cs
+++ ShopBanGiay/ShopBanGiay.Module/obj/Debug/net8.0/ShopBanGiay.Module.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShopBanGiay.Module")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc72063c27b89e70932b333b24352868c1c17e46")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83bfece0422ccbc634006efefcf2ab111a1ae7e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShopBanGiay.Module")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShopBanGiay.Module")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.*")]
